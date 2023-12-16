8cf5e4a01e35a8fa597fd56deb9eb7bf58486eb9
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-12-16 23:58:54 +0100

    Add general file structure and layout
    Add split background css for win/loose

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function boardWrapper() {
+    const board = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9']
+    ];
+        return {board};
+}
+const boardView = boardWrapper();
