dcc1e760c006bcd01740feda8cba2f7207c4f716
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-12-17 21:50:24 +0100

    Add game screen and players display

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
 const titleScreen = document.querySelector('.title-screen');
+const nameScreen = document.querySelector('.name-screen');
+const boardScreen = document.querySelector('.board-screen');
+const gameScreen = document.querySelector('.game-board');
+const playerNames = document.querySelector('.names');
+const nameInput = document.getElementById('name');
+const nameForm = document.getElementById('name-form');
 const playButton = document.getElementById('play');
 const startButton = document.getElementById('start-game');
 
@@ -7,9 +13,21 @@ playButton.addEventListener('click', () => {
     titleScreen.classList.add('fade');
     setTimeout(() => {
         titleScreen.classList.remove('visible');
-    }, 250)
-    
+    }, 300)
 });
+
+nameForm.addEventListener('submit', () => {
+    if (nameInput.value !== '') {
+        nameScreen.classList.add('fade');
+        setTimeout(() => {
+            nameScreen.classList.remove('visible');
+            gameScreen.classList.add('visible');
+            playerNames.classList.add('visible');
+        }, 300)
+    }
+    event.preventDefault();
+});
+
 
 // function boardWrapper() {
 //     const board = [
