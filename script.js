651b70d44dfcd75eb9de8762c1b0de0c9f55d7c6
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-12-17 16:59:17 +0100

    Add visual representation of game flow
    Add title screen and name input screen

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
+const titleScreen = document.querySelector('.title-screen');
+const playButton = document.getElementById('play');
+const startButton = document.getElementById('start-game');
 
-function boardWrapper() {
-    const board = [
-        ['1', '2', '3'],
-        ['4', '5', '6'],
-        ['7', '8', '9']
-    ];
-        return {board};
-}
-const boardView = boardWrapper();
+playButton.addEventListener('click', () => {
+    titleScreen.classList.add('fade');
+    setTimeout(() => {
+        titleScreen.classList.remove('visible');
+    }, 250)
+    
+});
+
+// function boardWrapper() {
+//     const board = [
+//         ['1', '2', '3'],
+//         ['4', '5', '6'],
+//         ['7', '8', '9']
+//     ];
+//         return {board};
+// }
+// const boardView = boardWrapper();
